bddfd874b63afc079b72f775b1931bf75390f0c6
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2015-03-07 15:42:42 -0500

    New: Foreign Keys
    Refs #2

--- part-2/question-2.sql
+++ part-2/question-2.sql
@@ -1,24 +1,24 @@
 CREATE TABLE Illness (
-    ill_name VARCHAR(64),
+    ill_name VARCHAR(64) PRIMARY KEY,
     contagious BOOLEAN,
     average_treatment_cost MONEY
 );
 
 CREATE TABLE Equipment (
-    eqpt_name VARCHAR(64),
+    eqpt_name VARCHAR(64) PRIMARY KEY,
     cost MONEY,
     consumable BOOLEAN
 );
 
 CREATE TABLE Department (
-    dept_name VARCHAR(64),
+    dept_name VARCHAR(64) PRIMARY KEY,
     address TEXT,
     budget MONEY,
     other_costs MONEY
 );
 
 CREATE TABLE Staff (
-    staff_id INTEGER,
+    staff_id INTEGER PRIMARY KEY,
     shift_to TIME,
     shift_from TIME,
     contact VARCHAR(64),
@@ -27,23 +27,23 @@ CREATE TABLE Staff (
 );
 
 CREATE TABLE Doctor (
-    staff_id INTEGER,
+    staff_id INTEGER PRIMARY KEY REFERENCES Staff(staff_id),
     rank VARCHAR(32),
     board_certification TEXT
 );
 
 CREATE TABLE Nurse (
-    staff_id INTEGER,
+    staff_id INTEGER PRIMARY KEY REFERENCES Staff(staff_id),
     certified_skills TEXT
 );
 
 CREATE TABLE Admin (
-    staff_id INTEGER,
+    staff_id INTEGER PRIMARY KEY REFERENCES Staff(staff_id),
     admin_responsibilities TEXT
 );
 
 CREATE TABLE Patient (
-    patient_id INTEGER,
+    patient_id INTEGER PRIMARY KEY,
     first_name VARCHAR(32),
     last_name VARCHAR(32),
     date_of_birth DATE,
@@ -52,26 +52,26 @@ CREATE TABLE Patient (
 );
 
 CREATE TABLE Has (
-    dept_name VARCHAR(64),
-    eqpt_name VARCHAR(64),
+    dept_name VARCHAR(64) REFERENCES Department(dept_name),
+    eqpt_name VARCHAR(64) REFERENCES Equipment(eqpt_name),
     amount_needed INTEGER,
     current_stock INTEGER
 );
 
 CREATE TABLE InChargeOf (
-    patient_id INTEGER,
+    patient_id INTEGER REFERENCES Patient(patient_id),
     dept_name VARCHAR(64),
     since DATE
 );
 
 CREATE TABLE SpecializesIn (
-    staff_id INTEGER,
+    staff_id INTEGER REFERENCES Staff(staff_id),
     ill_name VARCHAR(64)
 );
 
 CREATE TABLE SufferingFrom (
-    patient_id INTEGER,
-    ill_name VARCHAR(64),
+    patient_id INTEGER REFERENCES Patient(patient_id),
+    ill_name VARCHAR(64) REFERENCES Illness(ill_name),
     ill_since DATE,
     ill_until DATE,
     insurance_coverage MONEY,
@@ -80,14 +80,14 @@ CREATE TABLE SufferingFrom (
 );
 
 CREATE TABLE Treats (
-    staff_id INTEGER,
-    patient_id INTEGER,
+    staff_id INTEGER REFERENCES Staff(staff_id),
+    patient_id INTEGER REFERENCES Patient(patient_id),
     since DATE
 );
 
 CREATE TABLE WorksFor (
-    staff_id INTEGER,
-    dept_name VARCHAR(64),
+    staff_id INTEGER REFERENCES Staff(staff_id),
+    dept_name VARCHAR(64) REFERENCES Department(dept_name),
     wages MONEY,
     over_time FLOAT,
     contract_until DATE,
