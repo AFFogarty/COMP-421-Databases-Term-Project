5c33f60a3bbd90ac636b691eaf471f62131b8fa9
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2015-03-07 15:36:45 -0500

    New: Typed fields
    Refs #2

--- part-2/question-2.sql
+++ part-2/question-2.sql
@@ -1,96 +1,96 @@
 CREATE TABLE Illness (
-    ill_name,
-    contagious,
-    average_treatment_cost
+    ill_name VARCHAR(64),
+    contagious BOOLEAN,
+    average_treatment_cost MONEY
 );
 
 CREATE TABLE Equipment (
-    eqpt_name,
-    cost,
-    consumable
+    eqpt_name VARCHAR(64),
+    cost MONEY,
+    consumable BOOLEAN
 );
 
 CREATE TABLE Department (
-    dept_name,
-    address,
-    budget,
-    other_costs
+    dept_name VARCHAR(64),
+    address TEXT,
+    budget MONEY,
+    other_costs MONEY
 );
 
 CREATE TABLE Staff (
-    staff_id,
-    shift_to,
-    shift_from,
-    contact,
-    first_name,
-    last_name
+    staff_id INTEGER,
+    shift_to TIME,
+    shift_from TIME,
+    contact VARCHAR(64),
+    first_name VARCHAR(32),
+    last_name VARCHAR(32)
 );
 
 CREATE TABLE Doctor (
-    staff_id,
-    rank,
-    board_certification
+    staff_id INTEGER,
+    rank VARCHAR(32),
+    board_certification TEXT
 );
 
 CREATE TABLE Nurse (
-    staff_id,
-    certified_skills
+    staff_id INTEGER,
+    certified_skills TEXT
 );
 
 CREATE TABLE Admin (
-    staff_id,
-    admin_responsibilities
+    staff_id INTEGER,
+    admin_responsibilities TEXT
 );
 
 CREATE TABLE Patient (
-    patient_id,
-    first_name,
-    last_name,
-    date_of_birth,
-    care_cost,
-    dept_name
+    patient_id INTEGER,
+    first_name VARCHAR(32),
+    last_name VARCHAR(32),
+    date_of_birth DATE,
+    care_cost MONEY,
+    dept_name VARCHAR(64)
 );
 
 CREATE TABLE Has (
-    dept_name,
-    eqpt_name,
-    amount_needed,
-    current_stock
+    dept_name VARCHAR(64),
+    eqpt_name VARCHAR(64),
+    amount_needed INTEGER,
+    current_stock INTEGER
 );
 
 CREATE TABLE InChargeOf (
-    patient_id,
-    dept_name,
-    since
+    patient_id INTEGER,
+    dept_name VARCHAR(64),
+    since DATE
 );
 
 CREATE TABLE SpecializesIn (
-    staff_id,
-    ill_name 
+    staff_id INTEGER,
+    ill_name VARCHAR(64)
 );
 
 CREATE TABLE SufferingFrom (
-    patient_id,
-    ill_name,
-    ill_since,
-    ill_until,
-    insurance_coverage,
-    treatment_cost,
+    patient_id INTEGER,
+    ill_name VARCHAR(64),
+    ill_since DATE,
+    ill_until DATE,
+    insurance_coverage MONEY,
+    treatment_cost MONEY,
     urgency
 );
 
 CREATE TABLE Treats (
-    staff_id,
-    patient_id,
-    since
+    staff_id INTEGER,
+    patient_id INTEGER,
+    since DATE
 );
 
 CREATE TABLE WorksFor (
-    staff_id,
-    dept_name,
-    wages,
-    over_time,
-    contract_until,
-    salary,
-    contract_from
+    staff_id INTEGER,
+    dept_name VARCHAR(64),
+    wages MONEY,
+    over_time FLOAT,
+    contract_until DATE,
+    salary MONEY,
+    contract_from DATE
 );
