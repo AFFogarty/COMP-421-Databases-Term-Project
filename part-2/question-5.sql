d28542aea50b4b213622b0d4b2214d1e18078393
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2015-03-08 16:57:35 -0400

    New: Comments to keep track of which query is which
    Refs #5

--- part-2/question-5.sql
+++ part-2/question-5.sql
@@ -1,3 +1,5 @@
+/* Query 1 */
+
 SELECT first_name, last_name
 FROM Patient
 WHERE patient_id IN (SELECT patient_id
@@ -7,4 +9,17 @@ WHERE patient_id IN (SELECT patient_id
                     SELECT patient_id
                     FROM SufferingFrom
                     WHERE ill_name LIKE '%Norovirus%' AND ill_until IS NOT NULL);
+
+
+/* Query 2 */
+
+
+/* Query 3 */
+
+
+/* Query 4 */
+
+
+/* Query 5 */
+
 
