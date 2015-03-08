e9823133b28d8923d8726bacbd9bb8355e0a091a
Lillio MOK <lmok@teaching.CS.McGill.CA>
2015-03-08 17:00:43 -0400

    Fixed: forgot to remove conflict message

--- part-2/question-5.sql
+++ part-2/question-5.sql
@@ -1,8 +1,6 @@
 /* Query 1 */
 
 -- Get first name, last name from patients who have gastroenteritis and had norovirus before and recovered
-
->>>>>>> d28542aea50b4b213622b0d4b2214d1e18078393
 SELECT first_name, last_name
 FROM Patient
 WHERE patient_id IN (SELECT patient_id
