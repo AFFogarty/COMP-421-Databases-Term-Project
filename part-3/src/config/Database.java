a0b85f48693fb01ff1b592f5bd435f2b42082084
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2015-03-23 16:10:23 -0400

    New: renamed Database to DatabaseConfig

--- part-3/src/config/Database.java
+++
@@ -1,8 +0,0 @@
-package config;
-
-
-public class Database {
-
-    public static final String driver = "org.postgresql.Driver";
-    public static final String url = "jdbc:postgresql://db2.cs.mcgill.ca/CS421";
-}
