252078d6019fe57cf2ad43cb74bf315f235210d7
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2015-03-23 10:16:09 -0400

    New: Database configuration class

---
+++ part-3/src/config/Database.java
@@ -0,0 +1,8 @@
+package config;
+
+
+public class Database {
+
+    public static final String driver = "org.postgresql.Driver";
+    public static final String url = "jdbc:postgresql://db2.cs.mcgill.ca/CS421";
+}
