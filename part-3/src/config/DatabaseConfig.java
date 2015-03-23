8ce161bfb4e1b9909480a1a9b1b4a889152aba43
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2015-03-23 16:15:02 -0400

    Fixed: Actually included the new DatabaseConfig.java

---
+++ part-3/src/config/DatabaseConfig.java
@@ -0,0 +1,8 @@
+package config;
+
+
+public class DatabaseConfig {
+
+    public static final String driver = "org.postgresql.Driver";
+    public static final String url = "jdbc:postgresql://db2.cs.mcgill.ca/CS421";
+}
