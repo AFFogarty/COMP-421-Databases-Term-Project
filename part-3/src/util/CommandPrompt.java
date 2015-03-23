beec875d5d1776a531d6f4f93b427795bb14411b
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2015-03-23 09:41:27 -0400

    New: A method for prompting the user to enter Strings

---
+++ part-3/src/util/CommandPrompt.java
@@ -0,0 +1,25 @@
+package util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class CommandPrompt {
+
+    /**
+     * Prompt the user to enter a string
+     *
+     * @param name name of the variable
+     * @return the value
+     * @throws IOException
+     */
+    public static String getString(String name) throws IOException {
+        // Prompt the user
+        System.out.print("Please enter" + name + ": ");
+        // Open the reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // Return the value
+        return br.readLine();
+    }
+}
