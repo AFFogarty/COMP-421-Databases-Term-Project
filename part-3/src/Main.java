a21b9182ac6db52757eaa391a1cead3b0065a795
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2015-03-24 15:20:25 -0400

    New: Program divided into the commands

--- part-3/src/Main.java
+++ part-3/src/Main.java
@@ -1,28 +1,64 @@
+import art.Ascii;
 import util.CommandPrompt;
 import java.io.IOException;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 
 public class Main {
 
     public static void main(String[] args) {
+        // Set up the program
+        Database db = setUp();
 
-        String[] test = {"First", "Second", "Third"};
-        int t = -1;
-        try {
-            t = CommandPrompt.getMenuSelection(test);
-        } catch (IOException e) {
-            e.printStackTrace();
+        // Menu options
+        int menuSelection;
+        String[] test = {"First", "Second", "Third", "Fourth", "Fifth", "Quit"};
+        boolean running = true;
+        while (running) {
+            menuSelection = -1;
+            try {
+                menuSelection = CommandPrompt.getMenuSelection(test);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            switch (menuSelection) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                default:
+                    // End the program loop
+                    running = false;
+                    break;
+            }
         }
-        System.out.println(t);
 
-        Database db;
+        // Quit the program
+        quit(db);
+    }
+
+    /**
+     * Start up the program.
+     *
+     * @return the database that we will work with.
+     */
+    public static Database setUp() {
+        // Print the welcome message
+        System.out.println(Ascii.doubleSeparator);
+        System.out.println(Ascii.programName);
+        System.out.println(Ascii.singleSeparator + "\n");
+        System.out.println(Ascii.copyRight + "\n");
+        System.out.println(Ascii.doubleSeparator + "\n");
+
         try {
-            db = new Database();
-            ResultSet resultSet = db.executeQuery("SELECT * FROM PRODUCTS");
-            db.printResultSet(resultSet);
-            db.disconnect();
+            return new Database();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -30,8 +66,43 @@ public class Main {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        // If this happens, then something has gone wrong.
+        return null;
+    }
 
+    public static void firstQuery(Database db) {
+        // TODO
+    }
 
+    public static void secondQuery(Database db) {
+        // TODO
+    }
+
+    public static void thirdQuery(Database db) {
+        // TODO
+    }
+
+    public static void fourthQuery(Database db) {
+        // TODO
+    }
+
+    public static void fifthQuery(Database db) {
+        // TODO
+    }
+
+    /**
+     * Disconnect the DB and print a goodbye message.
+     * @param db
+     */
+    public static void quit(Database db) {
+        // Disconnect the db
+        try {
+            db.disconnect();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Terminating program.\n\n");
+        System.out.println(Ascii.goodBye);
     }
 
 }
