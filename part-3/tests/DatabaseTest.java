8992883d2a3518b8f455642b24ee85eb27eb8e5f
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2015-03-24 21:33:19 -0400

    New: Tests for the DB

---
+++ part-3/tests/DatabaseTest.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+
+
+import java.sql.ResultSet;
+
+import static org.junit.Assert.*;
+
+public class DatabaseTest {
+
+    @Test
+    public void testConnection() throws Exception {
+        // Connect
+        Database db = new Database();
+        // Make queries
+        ResultSet resultSet = db.executeQuery("SELECT * FROM staff");
+        db.executeQuery("SELECT * FROM department");
+        // Assert connected
+        assertTrue(db.isConnected());
+        // Clear the queries
+        db.closeResultSet(resultSet);
+        // Disconnect
+        db.disconnect();
+        // Assert disconnected
+        assertFalse(db.isConnected());
+    }
+
+}
