8d2359f85c166f75d5e54ed02c9616a75f12fdcf
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2015-03-26 15:15:15 -0400

    New: InsertPatientProcedure works

--- part-3/src/procedures/insertPatientProcedure.java
+++ part-3/src/procedures/insertPatientProcedure.java
@@ -27,13 +27,29 @@ public class InsertPatientProcedure {
             }
         } while(!validated);
 
-        ResultSet existing = db.executeQuery("SELECT * FROM Patient WHERE first_name like " + first_name + "last_name like " + last_name);
+        // Check if the patient already exists
+        ResultSet existing = db.executeQuery("SELECT * FROM Patient WHERE first_name like '"
+                + first_name + "' AND last_name like '" + last_name + "'");
         if(existing.next()){
             System.out.println("A patient with that first and last name already exists. Aborting.");
             return;
+        } else {
+            System.out.println("Patient " + last_name + ", " + first_name + " does not exist in the database.  Creating new record...");
+            // The patient doesn't exist, so we will create them.
+            String date_of_birth = CommandPrompt.getDateString("date of birth");
+            String care_cost = CommandPrompt.getNumberString("initial care cost");
+            // Construct the query that generates its own id
+            String insert_query = "INSERT INTO Patient VALUES((SELECT MAX(patient_id) + 1 FROM Patient), '"
+                    + first_name + "', '" + last_name + "', '" + date_of_birth + "', '" + care_cost + "')";
+            // Run the insert query
+            db.executeInsertUpdateDestroy(insert_query);
+            System.out.println(insert_query);
+            // It worked, so get the id number
+            ResultSet successfulPatientSet = db.executeQuery("SELECT patient_id FROM Patient WHERE first_name='"
+                    + first_name + "' AND last_name='" + last_name + "' AND date_of_birth='" + date_of_birth + "'");
+            successfulPatientSet.next();
+            int patient_id = successfulPatientSet.getInt("patient_id");
+            System.out.println("Patient " + last_name + ", " + first_name + " record created with id: " + patient_id + ".");
         }
-
-        
-
     }
 }
